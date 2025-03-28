4b67fc87aa43fff2a859c75fd1013c78a268cbe9
magusya <138338535+magusya@users.noreply.github.com>
2025-03-28 13:28:31 +0300

    commit_3

--- script_sambaDC.sh
+++ script_sambaDC.sh
@@ -6,6 +6,7 @@ apt-get install -y nano
 cd /etc
 rm -rf named.conf
 mv /home/git/education/named.conf /etc
+mv /home/git/education/resolved.conf /etc/systemd
 rm -f /etc/samba/smb.conf
 rm -rf /var/lib/samba
 rm -rf /var/cache/samba
@@ -14,8 +15,6 @@ systemctl stop bind
 systemctl disable bind
 systemctl start samba
 systemctl enable samba
-cd /etc/systemd
-sed -i 's/#DNSSubListener=yes/DNSStubListener=no/' resolved.conf
 systemctl restart systemd-resolved
 echo "
 Realm:BR-SRV.AU-TEAM.IRPO
