9a7319e0b3e3b599d85222d50725f35bf07b1131
magusya <138338535+magusya@users.noreply.github.com>
2025-04-13 17:14:28 +0300

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -1,4 +1,8 @@
 cd /etc/openssh
 rm -rf sshd_config
 cp /home/git/education/sshd_config /etc/openssh
+adduser sshuser 
+passwd sshuser
+usermod -aG wheel sshuser
+echo "sshuser ALL=(ALL) NOPASSWRD:ALL" >> /etc/sudoers
 systemctl start sshd
