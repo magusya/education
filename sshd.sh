1880a48eea2cd2d253262d143a1fb163f8155e1e
magusya <138338535+magusya@users.noreply.github.com>
2025-04-15 09:18:20 +0300

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -6,6 +6,7 @@ passwd sshuser
 usermod -aG wheel sshuser
 echo "sshuser ALL=(ALL) NOPASSWRD:ALL" >> /etc/sudoers
 systemctl start sshd
+ssh-keygen -t rsa
 ssh-copy-id user@192.168.0.65 -p 2024
 ssh-copy-id net_admin@192.168.0.62 -p 22
 ssh-copy-id net_admin@192.168.3.30
